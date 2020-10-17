9f7de9901fd16c451774fb500ed015eb14971207
AAGaming <aa@bigdumb.gq>
2020-10-17 18:27:07 -0400

    ruh boment

--- bruh.js
+++ bruh.js
@@ -1,6 +1,6 @@
 const fs = require('fs')
 var i;
-for (i = 2; i < 1000; i++) {
+for (i = 8; i < 10000; i++) {
     fs.writeFile(`${i}.css`, `@import url('https://adoesgit.github.io/100-level-theme/${i + 1}.css');`, (err) => { 
       
         // In case of a error throw err. 
