677b0355024ad73601c822038f1b22aa7c47944c
AAGaming <aa@bigdumb.gq>
2020-10-18 11:31:27 -0400

    fix

--- bruh.js
+++ bruh.js
@@ -1,5 +1,5 @@
 const fs = require('fs')
 var i;
-for (i = 8; i < 10000; i++) {
+for (i = 1; i < 10000; i++) {
     fs.writeFileSync(`${i}.css`, `@import url('https://adoesgit.github.io/100-level-theme/${i + 1}.css');`) 
 } 
